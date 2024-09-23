5728530ff975addacab1c888eb11e3781dc0e87c
unknown <shuklasumit93@gmail.com>
2024-09-23 20:16:13 +0530

    Added helper function in models

--- working-with-expressjs/models/product.js
+++ working-with-expressjs/models/product.js
@@ -2,6 +2,27 @@ const path = require('path');
 const fs = require('fs');
 // const product = []
 
+//path for file where we fetch or store data
+const p = path.join(__dirname,
+    '../',
+    'data',
+    'product.json');
+
+//helper function for fetching data storing data into files 
+
+const getProductFromFile = (cb) => {
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([])
+        }
+        else {
+            cb(JSON.parse(fileContent))
+        }
+    })
+
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t
@@ -11,37 +32,36 @@ module.exports = class Product {
     save() {
         // product.push(this)
         // console.log(product)
-        
-        const p = path.join(__dirname,
-            '../',
-            'data',
-            'product.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p , JSON.stringify(products) , err => {
-                console.log(err)
-            })
-        })
-    }
 
+        // const p = path.join(__dirname,
+        //     '../',
+        //     'data',
+        //     'product.json');
+        // fs.readFile(p, (err, fileContent) => {
+        //     let products = [];
+        //     if (!err) {
+        //         products = JSON.parse(fileContent);
+        //     }
+        //     products.push(this);
+        //     fs.writeFile(p , JSON.stringify(products) , err => {
+        //      console.log(err)
+        //     })
+        // })
+
+                                              //after adding helper function 
+        
+            getProductFromFile(product => {
+                product.push(this);
+                fs.writeFile(p , JSON.stringify(product) , err=>{
+                    console.log(err)
+                })
+            })
+
+    }
 
     static fetchAll(cb) {
         // return product;
-        const p = path.join(__dirname,
-            '../',
-            'data',
-            'product.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([])
-            }
-            
-            cb(JSON.parse(fileContent))
-        })
+        getProductFromFile(cb);
     }
 
 }
